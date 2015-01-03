13d5fc1533e4ad9785ed04f79e22a51a4b39dc9f
Tamas Papp <tamas.papp@rtfm.co.hu>
2015-01-03 01:23:14 +0100

    decreasing parallel jobs number to 2

--- parallel-backup.sh
+++ parallel-backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 vaults=`zfs list -H -o name -r tank/backup|grep -v ^tank/backup$|sed 's@^tank/backup/@@'`
-echo "$vaults" | parallel -j 3 --dry-run -a - zrb -f daily -v {1}
+echo "$vaults" | parallel -j 2 --dry-run -a - zrb -f daily -v {1}
 
