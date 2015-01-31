954298be8326369ffcc19ae0611ef617ffa9d623
Tamas Papp <tamas.papp@rtfm.co.hu>
2015-01-31 21:59:46 +0100

    parallel-backup.sh changes

--- parallel-backup.sh
+++ parallel-backup.sh
@@ -1,5 +1,55 @@
 #!/bin/bash
 
+
+f_check_switch_param(){
+	if echo x"$1" |grep -q ^x$;then
+		echo "Missing argument!"
+		exit 1
+	fi
+}
+
+f_usage(){
+	echo "Usage:"
+	echo "  $0             verbose output"
+	echo "  $0 -q          display vaults"
+	echo "  $0 -qq         full quiet"
+	echo
+}
+
+# Exit if no arguments!
+#let $# || { f_usage; exit 1; }
+
+while [ "$#" -gt "0" ]; do
+  case "$1" in
+	-q)
+		quiet_little=1
+		break
+	;;
+
+	-qq)
+		quiet_full=1
+		break
+	;;
+
+    ""$)
+        quiet_none=1
+        break
+	;;
+
+	-h|--help)
+		f_usage
+		exit 0
+	;;
+  esac
+done
+
+if tty > /dev/null;
+	then
+		interactive=1
+fi
+
+
+
 vaults=`zfs list -H -o name -r tank/backup|grep -v ^tank/backup$|sed 's@^tank/backup/@@'`
 echo "$vaults" | parallel -v -j 2 -a - zrb.sh -f daily -v {1}
 
